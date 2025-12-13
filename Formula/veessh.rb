8f9321c677438f848ba29808f51132d130aa2bd9
a1ex-var1amov <varlamov.it@gmail.com>
2025-12-12 21:49:03 -0500

    Initial formula for veessh

---
+++ Formula/veessh.rb
@@ -0,0 +1,42 @@
+# typed: false
+# frozen_string_literal: true
+
+class Veessh < Formula
+  desc "Console connection manager for SSH/SFTP/Telnet/Mosh/SSM/GCloud"
+  homepage "https://github.com/vee-sh/veessh"
+  version "0.3.0"
+  license "Apache-2.0"
+
+  on_macos do
+    on_intel do
+      url "https://github.com/vee-sh/veessh/releases/download/v#{version}/veessh_v#{version}_darwin_amd64.tar.gz"
+      sha256 "PLACEHOLDER_WILL_BE_UPDATED_BY_RELEASE_WORKFLOW"
+    end
+
+    on_arm do
+      url "https://github.com/vee-sh/veessh/releases/download/v#{version}/veessh_v#{version}_darwin_arm64.tar.gz"
+      sha256 "PLACEHOLDER_WILL_BE_UPDATED_BY_RELEASE_WORKFLOW"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/vee-sh/veessh/releases/download/v#{version}/veessh_v#{version}_linux_amd64.tar.gz"
+      sha256 "PLACEHOLDER_WILL_BE_UPDATED_BY_RELEASE_WORKFLOW"
+    end
+
+    on_arm do
+      url "https://github.com/vee-sh/veessh/releases/download/v#{version}/veessh_v#{version}_linux_arm64.tar.gz"
+      sha256 "PLACEHOLDER_WILL_BE_UPDATED_BY_RELEASE_WORKFLOW"
+    end
+  end
+
+  def install
+    bin.install "veessh"
+    generate_completions_from_executable(bin/"veessh", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/veessh --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,23 @@
+# Homebrew Tap for veessh
+
+This tap contains the Homebrew formula for [veessh](https://github.com/vee-sh/veessh).
+
+## Installation
+
+```bash
+brew tap vee-sh/tap
+brew install veessh
+```
+
+## Updating
+
+```bash
+brew update
+brew upgrade veessh
+```
+
+## About veessh
+
+veessh is a console connection manager supporting SSH, SFTP, Telnet, Mosh, AWS SSM, and GCP gcloud.
+
+For more information, see the [main repository](https://github.com/vee-sh/veessh).
